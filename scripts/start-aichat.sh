4ac7525fd4b58bcbd85e43258170d6677c61bcb0
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2025-10-05 16:56:40 -0400

    [SKIP-BUILD] Daemonize, Create log, Create PID, Use nobody as runtime user

--- scripts/start-aichat.sh
+++ scripts/start-aichat.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
 if [ -z "${AICHAT_CLI_ONLY}" ]; then
-	aichat --serve
+    if [ ! -f /var/run/aichat.pid ]; then
+        touch /var/run/aichat.pid
+    fi
+
+    if [ ! -f /var/log/aichat.log ]; then
+        touch /var/log/aichat.log
+    fi
+
+    chown nobody:nobody /var/run/aichat.pid
+    chown nobody:nobody /var/log/aichat.log
+
+    daemon \
+        -f \
+        -o /var/log/aichat.log \
+        -t "All-in-one LLM CLI Tool" \
+        -u nobody \
+        -p /var/run/aichat.pid \
+            aichat --serve
 fi
